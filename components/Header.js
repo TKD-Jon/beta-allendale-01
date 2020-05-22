78698a6da78e58d79cc7404cd63d13683187b5c1
TKD-Jon <46168629+TKD-Jon@users.noreply.github.com>
2020-05-22 11:30:33 -0500

    PWA should work

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,21 @@
-// Third Party
-const axios = require('axios').default;
+// Next.js imports
+import Head from 'next/head'
 
 function Header({ global_options }) {
     return (
         <header className="bg-gray-400">
+            <Head>
+            <meta charset='utf-8' />
+            <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+            <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+            <meta name='description' content="Testing PWA's for the Allendale Inc APP" />
+            <meta name='keywords' content='none' />
+            <title>Allendale | BETA PWA</title>
+
+            <link rel='manifest' href='/manifest.json' />
+            <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+            <meta name='theme-color' content='#3a5828' />
+            </Head>
             {console.dir(global_options)}
             <div className="
                 container 
